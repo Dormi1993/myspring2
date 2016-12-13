156cd3fc031aa6680dbc5f19907ab592cf51849d
dormi.fu <dormi.fu@vipshop.com>
2016-12-13 13:41:08 +0800

    Add Employee class.

---
+++ src/com/hsp/collection/Employee.java
@@ -0,0 +1,16 @@
+package com.hsp.collection;
+
+/**
+ * Created by dormi.fu on 2016/12/13.
+ */
+public class Employee {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
